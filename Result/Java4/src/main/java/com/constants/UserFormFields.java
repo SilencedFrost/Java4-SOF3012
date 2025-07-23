08517c7dd78d5f9bd7e7a97b570f7318d47b2937
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2025-07-23 21:21:34 +0700

    ready for auto-generated forms

--- Result/Java4/src/main/java/com/constants/UserFormFields.java
+++ Result/Java4/src/main/java/com/constants/UserFormFields.java
@@ -1,5 +1,6 @@
 package com.constants;
 
+import com.entity.User;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -7,24 +8,28 @@ import java.util.List;
 
 @Getter
 public enum UserFormFields {
-    USER_ID("userId", "User ID", "text", UserValidationError.USER_ID_REQUIRED, null),
-    PASSWORD("passwordHash", "Password", "password", UserValidationError.PASSWORD_REQUIRED, UserValidationError.PASSWORD_INCORRECT_FORMAT),
-    FULL_NAME("fullName", "Full Name", "text", UserValidationError.FULL_NAME_REQUIRED, null),
-    EMAIL("email", "Email", "email", UserValidationError.EMAIL_REQUIRED, UserValidationError.EMAIL_INCORRECT_FORMAT),
-    ROLE("roleName", "Role Name", "combobox", UserValidationError.ROLE_REQUIRED, null);
+    USER_ID("userId", "User ID", "text", UserValidationError.USER_ID_REQUIRED, null, null),
+    PASSWORD("passwordHash", "Password", "password", UserValidationError.PASSWORD_REQUIRED, UserValidationError.PASSWORD_INCORRECT_FORMAT, null),
+    FULL_NAME("fullName", "Full Name", "text", UserValidationError.FULL_NAME_REQUIRED, null, null),
+    EMAIL("email", "Email", "email", UserValidationError.EMAIL_REQUIRED, UserValidationError.EMAIL_INCORRECT_FORMAT, null),
+    ROLE("roleName", "Role Name", "combobox", UserValidationError.ROLE_REQUIRED, null, List.of("User", "Employee", "Manager", "Admin"));
 
     private final String propertyKey;
     private final String label;
+    private final String fieldType;
     private final UserValidationError requiredError;
     private final UserValidationError formatError;
+    private final List<String> cBoxData;
 
     private static final List<String> ALL_FIELD_KEYS = initializePropertyKeys();
 
-    UserFormFields(String propertyKey, String label, String fieldType, UserValidationError requiredError, UserValidationError formatError) {
+    UserFormFields(String propertyKey, String label, String fieldType, UserValidationError requiredError, UserValidationError formatError, List<String> cBoxData) {
         this.propertyKey = propertyKey;
         this.label = label;
+        this.fieldType = fieldType;
         this.requiredError = requiredError;
         this.formatError = formatError;
+        this.cBoxData = cBoxData;
     }
     private static List<String> initializePropertyKeys() {
         UserFormFields[] values = values();
@@ -48,4 +53,6 @@ public enum UserFormFields {
     public String errorKey() {
         return propertyKey + "Error";
     }
+
+
 }
